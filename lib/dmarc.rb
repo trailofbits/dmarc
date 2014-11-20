af7f755b2a5721a33fc8f63457b1634341af407b
Postmodern <postmodern.mod3@gmail.com>
2014-11-20 14:05:06 -0800

    Added DMARC.query method.

--- lib/dmarc.rb
+++ lib/dmarc.rb
@@ -2,3 +2,31 @@ require 'dmarc/exceptions'
 require 'dmarc/record'
 require 'dmarc/parser'
 require 'dmarc/version'
+
+require 'resolv'
+
+module DMARC
+  #
+  # Queries and parses the DMARC record for a domain.
+  #
+  # @param [String] domain
+  #   The domain to query DMARC for.
+  #
+  # @param [Resolv::DNS] resolver
+  #   The resolver to use.
+  #
+  # @return [Record]
+  #   The parsed DMARC record.
+  #
+  # @since 0.3.0
+  #
+  # @api public
+  def self.query(domain,resolver=Resolv::DNS.new)
+    subdomain = "_dmarc.#{domain}"
+    dmarc     = resolver.getresource(
+      subdomain, Resolv::DNS::Resource::IN::TXT
+    ).strings.join
+
+    Record.parse(dmarc)
+  end
+end
