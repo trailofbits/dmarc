506f30445bbbf3bce67f19cd88cf5650ba96569f
Postmodern <postmodern.mod3@gmail.com>
2014-12-03 17:17:41 -0800

    Fixed specs.

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -4,15 +4,15 @@ require 'dmarc/record'
 describe Record do
   context 'by default' do
     it 'has a relaxed DKIM alignment' do
-      expect(subject.adkim).to eq('r')
+      expect(subject.adkim).to eq(:r)
     end
 
     it 'has a relaxed SPF alignment' do
-      expect(subject.aspf).to eq('r')
+      expect(subject.aspf).to eq(:r)
     end
 
     it 'has failure reporting options of "0"' do
-      expect(subject.fo).to eq('0')
+      expect(subject.fo).to eq(['0'])
     end
 
     it 'has an application percentage of 100' do
@@ -20,7 +20,7 @@ describe Record do
     end
 
     it 'has an afrf report format' do
-      expect(subject.rf).to eq('afrf')
+      expect(subject.rf).to eq(:afrf)
     end
 
     it 'has a report interval of 1 day' do
@@ -29,24 +29,24 @@ describe Record do
   end
 
   describe '#initialize' do
-    let(:parse_tree) do
+    let(:attributes) do
       {
-        v: 'DMARC1',
-        p: 'none',
-        adkim: 'r'
+        v: :DMARC1,
+        p: :none,
+        adkim: :r
       }
     end
 
+    subject { described_class.new(attributes) }
+
     it 'assigns the fields to its properties' do
-      rec = described_class.new parse_tree
-      expect(rec.v).to eq('DMARC1')
-      expect(rec.p).to eq('none')
-      expect(rec.adkim).to eq('r')
+      expect(subject.v).to     be :DMARC1
+      expect(subject.p).to     be :none
+      expect(subject.adkim).to be :r
     end
 
     it 'gives "sp" the same value as "p" if undefined' do
-      rec = described_class.new parse_tree
-      expect(rec.sp).to eq('none')
+      expect(subject.sp).to be :none
     end
   end
 
